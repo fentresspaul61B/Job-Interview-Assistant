17bb8a47e5c3c347a9a4037d65a791ee58408737
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-08-25 14:33:40 -0700

    adding more comments.

--- main.py
+++ main.py
@@ -37,14 +37,19 @@ from st_helpers.UI_helpers import add_theme
 # Open AI used for whisper and chat GPT.
 import openai
 
-
+# Alters default text and background colors.
 add_theme()
 
+# Used to save user and chatbot dialog.
 DIALOG = []
 
+# Creating limit for job description length. 
+MAX_CHARS_JOB_DESCRIPT = 1000
+
+# Configires open AI settings.
 set_open_ai_token()
 
- # Instantiating ElevenLabs voice.
+# Instantiating ElevenLabs voice.
 my_voice = load_eleven_labs_voice()
 
 
@@ -52,22 +57,31 @@ def main():
     """
     Creates UI and runs dialog after password is confirmed.
     """
-    
-    st.title("Job Interview Dialog Model")
 
+    # Adding title. 
+    st.title("MLE Job Interview Assistant")
+
+    # Enter the password, which I will provide to users. 
     user_input = st.text_input("Enter Password", type="password")
 
+    # Only allow for interview if password is valid.
     if user_input == st.secrets["USER_PW"]:
         
-        
-        context = st.text_area("Enter Context")
-        
+        # Text box for job description.. 
+        context = st.text_area(
+            "Enter Job Description", 
+            max_chars=MAX_CHARS_JOB_DESCRIPT
+        )
+       
+        # Generate context for prompt, based on the job description.
         context = create_context(context)
-        # If the password is correct, then the dialog can start.
-
+        
+        # Instructions for recording audio.
         st.write("Press the button below to record audio.")
+        
         # Audio recording button. Have to set key manually, so widgets 
-        # do not conflict with eachother.
+        # do not conflict with eachother. Uses custom st package. May 
+        # need maintenence.
         audio_bytes = audio_recorder(
                 key="123", 
                 icon_name="square",
@@ -76,11 +90,9 @@ def main():
                 text=""
         )
            
-
         # Check if the session state has the 'processed' attribute
         if not hasattr(st.session_state, 'processed'):
             st.session_state.processed = False
-
 
         # Only process if the 'processed' flag is not set
         if audio_bytes and not st.session_state.processed:
@@ -92,32 +104,27 @@ def main():
             # Reading from the file and translating
             with open("TTS.wav", mode="rb") as f:  
                 response = openai.Audio.translate("whisper-1", f)["text"]
-            
-            
-            # Creating audio byte string.
 
+            # Saving dialog. 
             DIALOG.append({"user": response})
-            
+           
+            # Configuring the chat conversation object.
             LANG_CHAIN_CONVERSATION = configure_lang_chain(context)
 
-
+            # Generating response with context and memory.
             chatbot_response = get_chat_lang_chain_response(
                 response,
                 lang_chain_conversation=LANG_CHAIN_CONVERSATION
             )
-     
-            # chatbot_response = generate_chat_response(response)
-
+    
+            # Saving chat response.
             DIALOG.append({"chat_bot": chatbot_response})
 
-            # st.chat_message(chatbot_response)
-
+            # Generate TTS with eleven labs.
             audio = generate_eleven_labs_audio(chatbot_response, my_voice)
             
-            
-            # st.write(DIALOG)
-
-            # Custom auto play function, made for streamlit.   
+            # Play the audio using custom auto play function, made for 
+            # streamlit.   
             autoplay_audio_from_bytes(audio)
            
             # Convert audio bytes into .wav.
@@ -128,6 +135,7 @@ def main():
             # finished playing.
             sleep_time = get_audio_duration("STT.wav")
 
+            # Pause to refresh until output audio is completed.
             time.sleep(sleep_time)
 
             # Set the 'processed' flag
@@ -135,8 +143,7 @@ def main():
 
             # Rerun the app
             st.experimental_rerun()
-
-
+    
         # Reset audio_bytes and processed flag for the next interaction
         audio_bytes = None
         st.session_state.processed = False
@@ -144,3 +151,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
