afe73cfba14c9347608cd178f4726dedd085d458
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-08-25 18:00:40 -0700

    adding it again.

--- Home.py
+++ Home.py
@@ -1,6 +1,11 @@
+"""
+Landing page shows a description of the app, and explains how to use it.
+"""
+
 import streamlit as st 
 
 from st_helpers.UI_helpers import add_theme
+
 from streamlit_extras.switch_page_button import switch_page
 
 
@@ -9,7 +14,10 @@ add_theme(add_gradient=True)
 st.title("Home Page")
 
 start_interview = st.button("Start the Interview!")
+
 if start_interview:
     switch_page("Interview")
 
+
+add_theme(add_gradient=True)
 
