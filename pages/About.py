e13033be9a4aca8b5b9212b09d5e041f4dc3ad79
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-08-25 20:55:35 -0700

    fixing text.

--- pages/About.py
+++ pages/About.py
@@ -13,12 +13,19 @@ st.title("MLE Interview Assistant")
 st.header("How It Works")
 
 st.write(
-"""Single shot prompt engineering was applied, along with summarized memory in order to guide the LLM to role play as an interviewer, and ask relevant questions in a back and forth dialog format. 
+"""Single shot prompt engineering was applied, along with summarized memory in order to guide the LLM to role play as an interviewer, and ask relevant questions in a back and forth dialog format."""
 
-A. User Speaks into APP
-B. Whisper translates speech to text.
-C. Text from user is turned into a prompt using LangChain. and response is generated using GPT-4. 
-D. Response is converted from text to speech using ElevenLabs."""
-)
+
+st.write("A. User Speaks into APP.")
+
+#
+st.write("B. Whisper translates speech to text.")
+
+#
+st.write("C. Text from user is turned into a prompt using LangChain. and response is generated using GPT-4.") 
+
+#
+st.write("D. Response is converted from text to speech using ElevenLabs.")
+
 #
 st.image("images/MLE_interview_diagram.png")
