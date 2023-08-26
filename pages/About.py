4b7d650912370c48ff4439173171da38766a4469
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-08-25 20:45:59 -0700

    adding about.

--- pages/About.py
+++ pages/About.py
@@ -1,3 +1,24 @@
 import streamlit as st 
 
+from st_helpers.UI_helpers import add_theme
+
+
+# Alters default text and background colors.
+add_theme(add_gradient=True)
+
+# 
+st.title("MLE Interview Assistant")
+
+#
+st.header("How It Works")
+
+st.write(
+"""Single shot prompt engineering was applied, along with summarized memory in order to guide the LLM to role play as an interviewer, and ask relevant questions in a back and forth dialog format. 
+
+A. User Speaks into APP
+B. Whisper translates speech to text.
+C. Text from user is turned into a prompt using LangChain. and response is generated using GPT-4. 
+D. Response is converted from text to speech using ElevenLabs."""
+)
+#
 st.image("images/MLE_interview_diagram.png", use_column_width=False)
