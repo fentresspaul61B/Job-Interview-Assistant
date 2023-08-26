9ff97bb8490214f19e082446463a11c6b0ce48f7
fentresspaul61B <77761564+fentresspaul61B@users.noreply.github.com>
2023-08-25 18:18:19 -0700

    the gradient was just super light.

--- st_helpers/UI_helpers.py
+++ st_helpers/UI_helpers.py
@@ -205,7 +205,7 @@ def add_theme(add_gradient=False):
 
     # 1. Change font.
     apply_font_to_st_page()
-     
-    apply_gradient_to_st_page(TEST_GRADIENT)
+    if add_gradient: 
+        apply_gradient_to_st_page(TEST_GRADIENT)
     
 
